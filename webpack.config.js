29177a2eb191fb22d92dc7046387a757d418e54e
mts4 <matias04041994@gmail.com>
2020-10-31 14:41:42 -0300

    Se integra en produccion

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,131 @@
-const path = require('path');
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-    entry: './src/javascript/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.min.js",
+  },
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.handlebars$/,
+        loader: "handlebars-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "resolve-url-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browser: ["last 2 versions"],
+              },
+              plugins: () => [autoprefixer]
             },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader"
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
-                    }
-                ]
+          },
+          
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              includePaths: ["./node_modules"]
             },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'images'
-                        }
-                    }
-                ]
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./img/",
             },
-            {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'fonts'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
-        })
-    ]
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 90,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.9, 0.95],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 90,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              publicPath: "./fonts/",
+              outputPath: "fonts",
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].min.css",
+      chunkFilename: "[id].min.css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Lengüix - Trendy",
+      template: "./src/index.handlebars",
+      minify: {
+        html5: true,
+        collapseWhitespace: true,
+        caseSensitive: true,
+        removeComments: true,
+        removeEmptyElements: true,
+      },
+    }),
+  ],
 };
